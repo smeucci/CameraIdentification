ed8453ccbca1245fd1840aeb295346b8385d6bb0
Lorenzo Cioni <lore.cioni@gmail.com>
2016-05-06 16:13:40 +0200

    pie edistance

--- matlab/utils/PCEdistance.m
+++ matlab/utils/PCEdistance.m
@@ -1,12 +1,8 @@
-function distances = PCEdistance(imgx, imgy)
+function affinity = PCEdistance(imgx, imgy)
 %Evaluate the PCE distance between imgx and imgy (both PRNU noises)
-    distances = zeros(size(imgy, 1), 1);
-    for i = 1:size(imgy, 1)
-        noisex = imgx.PRNU;
-        noisey = imgy(i).PRNU;
-        C = crosscorr(noisex, noisey);
-        detection = PCE(C);
-        distances(i) = max(0, detection.PCE);     
-    end
+    noisex = imgx.PRNU;
+    noisey = imgy.PRNU;
+    C = crosscorr(noisex, noisey);
+    detection = PCE(C);
+    affinity = detection.PCE;
 end
-
