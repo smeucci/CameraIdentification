b06bad815265f98347c9315d125ec455b562c00c
Lorenzo Cioni <lore.cioni@gmail.com>
2016-05-12 12:39:15 +0200

    crosscorr

--- matlab/utils/PCEdistance.m
+++ matlab/utils/PCEdistance.m
@@ -1,6 +1,6 @@
 function affinity = PCEdistance(noisex, noisey)
 %Evaluate the PCE distance between imgx and imgy (both PRNU noises)
-    C = normxcorr2(noisex, noisey);
+    C = crosscorr(noisex, noisey);
     detection = PCE(C);
     affinity = detection.PCE;
 end
