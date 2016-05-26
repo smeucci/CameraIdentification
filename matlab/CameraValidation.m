7dbd06581ccc9a6d339933dc22fe779595df088c
Lorenzo <lore.cioni@gmail.com>
2016-05-26 12:35:14 +0200

    camera validation fixes

--- matlab/CameraValidation.m
+++ matlab/CameraValidation.m
@@ -7,7 +7,7 @@ function [camera, affinity] = CameraValidation(image)
     addpath('Filter');
     addpath('rwt-master/bin');
     
-    fprintf('  -  Image %s\n', image);
+    fprintf('  - Image %s\n', image);
 
     camera_folder = dir('mat/cameras/');
     n_cameras = length(camera_folder) - 2;
@@ -15,26 +15,31 @@ function [camera, affinity] = CameraValidation(image)
     pce = zeros(length(n_cameras), 1);
     cameras = cell(length(n_cameras), 1);
     
+    I = double(rgb2gray(imread(image)));
+    %Resize image if different size from fingerprint
+    
+         
+    PRNU = NoiseExtractFromImage(image, 2);
+    PRNU = WienerInDFT(PRNU, std2(PRNU));
+    
     for i = 1:n_cameras
        load(['mat/cameras/camera_' num2str(i) '.mat'], 'camera'); 
        height = size(camera.fingerprint, 1);
        width = size(camera.fingerprint, 2);
        
-       I = double(rgb2gray(imread(image)));
-       %Resize image if different size from fingerprint
+       tmpImg = I;
+       tmpPRNU = PRNU;
+       
        if(size(I, 1) ~= height || size(I, 2) ~= width)
-           I = imresize(I, [height width]);
+           tmpImg = imresize(I, [height width]);
        end  
-         
-       PRNU = NoiseExtractFromImage(image, 2);
-       PRNU = WienerInDFT(PRNU, std2(PRNU));
        
        %Resize prnu if different size from fingerprint
        if(size(PRNU, 1) ~= height || size(PRNU, 2) ~= width)
-           PRNU = imresize(PRNU, [height width]);
+           tmpPRNU = imresize(PRNU, [height width]);
        end  
        
-       C = crosscorr(PRNU, I .* camera.fingerprint);
+       C = crosscorr(tmpPRNU, tmpImg .* camera.fingerprint);
        detection = PCE(C);
        pce(i) = detection.PCE;
        cameras{i}.pce = pce(i);
@@ -42,8 +47,9 @@ function [camera, affinity] = CameraValidation(image)
        fprintf('    -Compared with camera: %d\n', i);
     end
     
+    cameras = cell2mat(cameras);
     [affinity, index] = max(pce);
-    camera = cameras{index}.folder;
+    camera = cameras(index).folder;
     
 end
 
