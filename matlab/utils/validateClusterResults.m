61b05d929dc9dc3237388bd64e04453bb0c4676e
Lorenzo <lore.cioni@gmail.com>
2016-06-10 11:18:07 +0200

    fix on validate cluster results

--- matlab/utils/validateClusterResults.m
+++ matlab/utils/validateClusterResults.m
@@ -1,45 +1,46 @@
-function [precision, recall, accuracy, fpr] = validateClusterResults(images, clusters)
+function [precision, recall, accuracy, fpr, tpr] = validateClusterResults(images, clusters)
 %Evaluate clustering results
 
+    tpr = 0;
+    fpr = 0;
     precisions = zeros(length(clusters), 1);
     recalls = zeros(length(clusters), 1);
     accuracies = zeros(length(clusters), 1);
-    fprs = zeros(length(clusters), 1);
     
     total_labels = unique({images.folder});
-    for l = 1:length(total_labels)
-        gt_label = find( cellfun(@(x) strcmp(x, total_labels(l)), {images.folder}));
-        intersections = zeros(length(clusters), 1);
-        for i = 1:length(clusters)
-            clust = clusters{i};
-            I = intersect(clust, gt_label);
-            intersections(i) = length(I);
+    for i = 1:length(clusters)
+        intersections = zeros(length(total_labels), 1);
+        for l = 1:length(total_labels)
+            gt_label = find( cellfun(@(x) strcmp(x, total_labels(l)), {images.folder}));
+            I = intersect(clusters{i}, gt_label);
+            intersections(l) = length(I);
         end
         
         [best, idx] = max(intersections);
-        dim = length(clusters{idx});
-        if best ~= dim
-            tp = best;
-            fp = dim - tp;
-        else
-            tp = dim;
-            fp = 0;
-        end  
-        fn = length(gt_label) - tp;
-        tn = length(images) - dim - fn;
+        gt_label = find( cellfun(@(x) strcmp(x, total_labels(idx)), {images.folder}));
+        dim = length(gt_label);
         
+        tp = best;
+        fp = length(clusters{i}) - tp;
+        fn = dim - tp;
+        tn = length(images) - dim - fn;
+
         %Storing measures
         precisions(l) = tp / (tp + fp);
         recalls(l) = tp / (tp + fn);
         accuracies(l) = (tp + tn) / (tp + tn + fp + fn);
-        fprs(l) = fp / (tp + fp);
+        
+        tpr = tpr + tp;
+        fpr = fpr + fp;
         
     end
-
+    
     %Means of precisions, recalls and accuracies of each cluster
     precision = mean(precisions);
     recall = mean(recalls);
     accuracy = mean(accuracies);
-    fpr = mean(fprs);
+    
+    tpr = tpr / length(images);
+    fpr = fpr / length(images);
 end
 
