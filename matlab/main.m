3dadf33c23fa7fc218e94ff0c9ea44e68b88c787
Lorenzo <lore.cioni@gmail.com>
2016-06-14 09:20:01 +0200

    main

--- matlab/main.m
+++ matlab/main.m
@@ -9,16 +9,28 @@ addpath('Filter');
 addpath('rwt-master/bin');
 addpath('ncuts');
 
-imgpath = '../dataset';
+imgpath = '../dataset/images/';
 type = 'imgs_nat';
 numImages = 50;
-numFolders = 14;
+numFolders = 12;
 random = false;
+outputPath = 'crossvalidation_imgs_nat/';
 
 fprintf('CameraIdentification main script\n\n');
 
-CameraIdentification(imgpath, type, 'NumFolders', numFolders, 'NumImages', ...
-    numImages, 'ExtractNoise', true, 'Random', random);
+%try
+%load('mat/imgs_nat_fb_highres/images_weights.mat', 'weights');
+weights = weights(1:5*50, 1:5*50);
+[tprs, fprs, thresholds] = crossvalidateThreshold(imgpath, type, 6, 30, w, 0:10^-5:10^-2, 'NC', outputPath);
+
+%    CameraIdentification(imgpath, type, 'NumFolders', numFolders, 'NumImages', ...
+%        numImages, 'ExtractNoise', false, 'Random', random, 'OutputPath', outputPath);
+    
+%    type = 'imgs_nat_fb_highres';
+%    outputPath = [type '/'];
+    
+%    CameraIdentification(imgpath, type, 'NumFolders', numFolders, 'NumImages', ...
+%        numImages, 'ExtractNoise', true, 'Random', random, 'OutputPath', outputPath);
 
 %load('mat/images_weights.mat', 'weights');
 %[th, acc] = crossvalidateThreshold(imgpath, 14, 30, weights, 0:10^-6:10^-3, 'NC');
@@ -30,8 +42,9 @@ CameraIdentification(imgpath, type, 'NumFolders', numFolders, 'NumImages', ...
 %CameraIdentification(imgpath, type, 'NumFolders', numFolders, 'NumImages', ...
 %    numImages, 'ExtractNoise', true, 'Random', random);
 
-confusionMatrix = Validation(imgpath, 'imgs', 'NumFolders', 14, 'NumImages', 10);
-save('mat/confusionMatrix.mat', 'confusionMatrix');
-
-
-exit;
+    %confusionMatrix = Validation(imgpath, type, 'NumFolders', numFolders, 'NumImages', 10, 'OutputPath', outputPath);
+    %save('mat/confusionMatrix.mat', 'confusionMatrix');
+%catch
+%   disp('errore'); 
+%end
+%exit;
